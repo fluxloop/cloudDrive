ff4253a8bcc32601e4967a18657a43eb9b76ee80
Morten Helland <mort3nh@gmail.com>
2015-03-04 13:02:34 +0100

    ->3.4

--- www/main.js
+++ www/main.js
@@ -9,7 +9,7 @@ var DiscountCodes = [];
 var ComplaintCodes = [];
 var lastOrderId = 0;
 var lastPaymentCode = "";
-var currentversion='3.3';
+var currentversion='3.4';
 
 //Payment methods
 var paymentMethodCash = "K";
